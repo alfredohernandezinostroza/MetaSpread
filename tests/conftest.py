636cde9ce667bd921a6dbb53b98a1d6522636e5c
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2024-12-20 18:41:25 +0000

    modify random seed fixture or tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,10 @@ import random as rand
 import pytest
 
 # We need to set a seed for consistency
-# This seed specifically putsno no more that 2 cancercells per gridcell,
+# This seed specifically puts no more that 2 cancercells per gridcell,
 # allowing to test proliferation
 @pytest.fixture
 def random():
-    rand.seed(1) 
-    numpy.random.seed(0)
+    rand.seed(1)
+    random.getstate()[1][0]
+     
