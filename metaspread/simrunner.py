37221451aef4eddfbaafb93a902387d53085e121
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2024-12-20 20:35:12 +0000

    now model  is returned

--- metaspread/simrunner.py
+++ metaspread/simrunner.py
@@ -86,3 +86,4 @@ def run_simulation(max_steps, data_collection_period, loaded_simulation_path="")
     for i in range(max_steps):
         model.step()
     print(f'Finished the simulation at time step {model.schedule.time}!')
+    return model
