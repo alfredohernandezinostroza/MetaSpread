56dcb3ed661ed427d6d3728215d9a78d93858ca1
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2024-12-22 22:42:13 +0000

    add loading test to cancermodel

--- tests/test_cancermodel.py
+++ tests/test_cancermodel.py
@@ -1,6 +1,8 @@
 import pytest
 from metaspread import cancermodel
 from metaspread import configs
+from metaspread import simrunner
+from pathlib import Path
 
 def test_imports():
     assert cancermodel.mesa is not None
@@ -29,6 +31,49 @@ import ast
 
 #todo: model is not callable (duh! I think I cannot call a private variable (is it though?))
 #todo: use tmp_path_facorty to create the model once, and use it for the rest of the tests
+
+def test_load(tmp_path) -> None:
+    simulations_path = tmp_path / "Simulations"
+    model = simrunner.run_simulation(10, 10, save_path=tmp_path)
+    
+    model_loaded = CancerModel(
+        number_of_initial_cells=30,
+        width=201,
+        height=201,
+        grids_number=3,
+        max_steps=1000,
+        data_collection_period=2,
+        new_simulation_folder= simulations_path / "test_loaded_sim",
+        loaded_simulation_path = simulations_path / "Sim-max_steps-10-collection_period-10-cells-388-grids_number-3")
+    assert model.vasculature == model_loaded.vasculature
+    assert model.number_of_initial_cells == model_loaded.number_of_initial_cells
+    assert model.width == model_loaded.width
+    assert model.height == model_loaded.height
+    assert model.phenotypes == model_loaded.phenotypes
+    assert model.grid_vessels_positions == model_loaded.grid_vessels_positions
+    assert model.current_agent_id == model_loaded.current_agent_id
+    assert model.grids_number == model_loaded.grids_number
+    assert model.grid_ids == model_loaded.grid_ids
+    assert model.cancer_cells_counter == model_loaded.cancer_cells_counter
+    assert model.time_grid_got_populated == model_loaded.time_grid_got_populated
+    
+    for i in range(1,model.grids_number):
+        print(i)
+        assert model.grid_vessels_positions[i] == model_loaded.grid_vessels_positions[i]
+    
+    for i in range(model.grids_number):
+        assert (np.abs(model.mmp2[i][0] - model_loaded.mmp2[i][0]) < 10e-16).all()
+        assert (np.abs(model.ecm[i][0] - model_loaded.ecm[i][0]) < 10e-16).all()
+    
+    assert model.max_steps != model_loaded.max_steps
+    # right now the cells are counted on each step, so loading these cell counts
+    # from the previous model is not implemented, therefore this test is not necessary.
+    # But in the future that optimization should be implemented and thus,
+    # this tests should be added 
+    # for i in range(model.grids_number):
+    #     assert (model.mesenchymal_count[i] == model_loaded.mesenchymal_count[i]).all()
+    #     assert (model.epithelial_count[i] == model_loaded.epithelial_count[i]).all()
+
 
 def test_cancermodel(tmp_path) -> None:
     temp_simulation_folder = tmp_path / "Simulations" 
