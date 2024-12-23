42c5b3f5d755436e3fb5d4e9231edbcc0b31395f
Alfredo Hernandez <alfredo.hernandez@ug.uchile.cl>
2024-12-23 12:42:01 +0000

    add est for cell movement in all directions

--- tests/test_cancercell.py
+++ tests/test_cancercell.py
@@ -50,7 +50,7 @@ def test_cancercell(tmp_path) -> None:
     assert ccell.phenotype          == "mesenchymal"
     assert np.array_equal(ccell.ecm, model.ecm[grid_id-1])
     assert np.array_equal(ccell.mmp2, model.mmp2[grid_id-1])
-def test_cancercell_movement(tmp_path) -> None:
+def test_cancercell_movement_left(tmp_path) -> None:
     temp_simulation_folder = tmp_path / "test_simulation_movement"
     temp_simulation_folder.mkdir()
     model = CancerModel(
@@ -95,4 +95,191 @@ def test_cancercell_movement(tmp_path) -> None:
         for agent in model.schedule.agents:
             if agent.agent_type == "cell":
                 assert agent.pos == future_positions[i]
+                i = i + 1
+
+def test_cancercell_movement_right(tmp_path) -> None:
+    temp_simulation_folder = tmp_path / "test_simulation_movement"
+    temp_simulation_folder.mkdir()
+    model = CancerModel(
+        number_of_initial_cells=0,
+        width=201,
+        height=201,
+        grids_number=2,
+        max_steps=1000,
+        data_collection_period=10,
+        new_simulation_folder=temp_simulation_folder,
+        fixed_p_left=0,
+        fixed_p_right=1,
+        fixed_p_top=0,
+        fixed_p_bottom=0
+        )
+
+    grid_id = 1
+    ccell = CancerCell(
+        unique_id=model.current_agent_id,
+        model=model, 
+        grid=model.grids[grid_id-1], 
+        grid_id=grid_id, 
+        phenotype="mesenchymal", 
+        ecm=model.ecm[grid_id-1], 
+        mmp2=model.mmp2[grid_id-1]
+    )
+    x = 100
+    y = 100
+    model.current_agent_id += 1
+    model.grids[grid_id-1].place_agent(ccell, (x,y)) 
+    model.cancer_cells_counter[grid_id-1] += 1
+    model.schedule.add(ccell)
+    
+    for j in range(1,10):
+        current_positions = []
+        for agent in model.schedule.agents:
+            if agent.agent_type == "cell":
+                current_positions.append(agent.pos)
+        model.step()
+        future_positions = [(pos[0]+1,pos[1]) for pos in current_positions]
+        i = 0
+        for agent in model.schedule.agents:
+            if agent.agent_type == "cell":
+                assert agent.pos == future_positions[i]
+                i = i + 1
+
+def test_cancercell_movement_down(tmp_path) -> None:
+    temp_simulation_folder = tmp_path / "test_simulation_movement"
+    temp_simulation_folder.mkdir()
+    model = CancerModel(
+        number_of_initial_cells=0,
+        width=201,
+        height=201,
+        grids_number=2,
+        max_steps=1000,
+        data_collection_period=10,
+        new_simulation_folder=temp_simulation_folder,
+        fixed_p_left=0,
+        fixed_p_right=0,
+        fixed_p_top=0,
+        fixed_p_bottom=1
+        )
+
+    grid_id = 1
+    ccell = CancerCell(
+        unique_id=model.current_agent_id,
+        model=model, 
+        grid=model.grids[grid_id-1], 
+        grid_id=grid_id, 
+        phenotype="mesenchymal", 
+        ecm=model.ecm[grid_id-1], 
+        mmp2=model.mmp2[grid_id-1]
+    )
+    x = 100
+    y = 100
+    model.current_agent_id += 1
+    model.grids[grid_id-1].place_agent(ccell, (x,y)) 
+    model.cancer_cells_counter[grid_id-1] += 1
+    model.schedule.add(ccell)
+    
+    for j in range(1,10):
+        current_positions = []
+        for agent in model.schedule.agents:
+            if agent.agent_type == "cell":
+                current_positions.append(agent.pos)
+        model.step()
+        future_positions = [(pos[0],pos[1]-1) for pos in current_positions]
+        i = 0
+        for agent in model.schedule.agents:
+            if agent.agent_type == "cell":
+                assert agent.pos == future_positions[i]
+                i = i + 1
+
+def test_cancercell_movement_up(tmp_path) -> None:
+    temp_simulation_folder = tmp_path / "test_simulation_movement"
+    temp_simulation_folder.mkdir()
+    model = CancerModel(
+        number_of_initial_cells=0,
+        width=201,
+        height=201,
+        grids_number=2,
+        max_steps=1000,
+        data_collection_period=10,
+        new_simulation_folder=temp_simulation_folder,
+        fixed_p_left=0,
+        fixed_p_right=0,
+        fixed_p_top=1,
+        fixed_p_bottom=0
+        )
+
+    grid_id = 1
+    ccell = CancerCell(
+        unique_id=model.current_agent_id,
+        model=model, 
+        grid=model.grids[grid_id-1], 
+        grid_id=grid_id, 
+        phenotype="mesenchymal", 
+        ecm=model.ecm[grid_id-1], 
+        mmp2=model.mmp2[grid_id-1]
+    )
+    x = 100
+    y = 100
+    model.current_agent_id += 1
+    model.grids[grid_id-1].place_agent(ccell, (x,y)) 
+    model.cancer_cells_counter[grid_id-1] += 1
+    model.schedule.add(ccell)
+    
+    for j in range(1,10):
+        current_positions = []
+        for agent in model.schedule.agents:
+            if agent.agent_type == "cell":
+                current_positions.append(agent.pos)
+        model.step()
+        future_positions = [(pos[0],pos[1]+1) for pos in current_positions]
+        i = 0
+        for agent in model.schedule.agents:
+            if agent.agent_type == "cell":
+                assert agent.pos == future_positions[i]
+                i = i + 1
+
+def test_cancercell_movement_stay(tmp_path) -> None:
+    temp_simulation_folder = tmp_path / "test_simulation_movement"
+    temp_simulation_folder.mkdir()
+    model = CancerModel(
+        number_of_initial_cells=0,
+        width=201,
+        height=201,
+        grids_number=2,
+        max_steps=1000,
+        data_collection_period=10,
+        new_simulation_folder=temp_simulation_folder,
+        fixed_p_left=0,
+        fixed_p_right=0,
+        fixed_p_top=0,
+        fixed_p_bottom=0
+        )
+
+    grid_id = 1
+    ccell = CancerCell(
+        unique_id=model.current_agent_id,
+        model=model, 
+        grid=model.grids[grid_id-1], 
+        grid_id=grid_id, 
+        phenotype="mesenchymal", 
+        ecm=model.ecm[grid_id-1], 
+        mmp2=model.mmp2[grid_id-1]
+    )
+    x = 100
+    y = 100
+    model.current_agent_id += 1
+    model.grids[grid_id-1].place_agent(ccell, (x,y)) 
+    model.cancer_cells_counter[grid_id-1] += 1
+    model.schedule.add(ccell)
+    
+    for j in range(1,10):
+        current_positions = []
+        for agent in model.schedule.agents:
+            if agent.agent_type == "cell":
+                current_positions.append(agent.pos)
+        model.step()
+        i = 0
+        for agent in model.schedule.agents:
+            if agent.agent_type == "cell":
+                assert agent.pos == current_positions[i]
                 i = i + 1
